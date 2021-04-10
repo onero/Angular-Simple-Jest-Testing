dfb566abce23acee5d26e10808ad5f3649ce3599
Adam Hansen <onero@users.noreply.github.com>
2021-04-11 00:36:27 +0200

    Updated component to use new service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyTestService } from './my-test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'simple-jest-testing';
+
+   constructor(private myTestServiceMock: MyTestService) {}
+
+   ngOnInit() {
+    this.doComponentStuff();
+    this.myTestServiceMock.doServiceStuff()
+  }
+
+  doComponentStuff() {
+    // Left empty on purpose
+  }
 }
